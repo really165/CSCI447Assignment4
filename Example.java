61ffbe1791bd66afc7617ded02031071821a64d3
Jaret Boyer <jaret.c.boyer@gmail.com>
2019-12-08 14:12:28 -0700

    Removed the distance function and comparisons in the example class used for nearest neighbor.

--- Example.java
+++ Example.java
@@ -1,18 +1,13 @@
 package csci447.project4;
 
-import org.ejml.simple.SimpleMatrix;
-
  /**
   * Stores the data for each dataset
   */
- public final class Example implements Comparable<Example> {
-
-    public SimpleMatrix inputs;
-    public SimpleMatrix target;
+ public final class Example {
 
     public double c;
     public double[] input;
-    public double[] targeta;
+    public double[] target;
     public double distance;
 
     /**
@@ -21,13 +16,10 @@ import org.ejml.simple.SimpleMatrix;
      * @param target
      * @param numClasses
      */
-    public Example(double[] inputs, int target, int numClasses) {
-        this.input = inputs;
-        this.targeta = new double[numClasses];
-        this.targeta[target] = 1;
-        this.inputs = new SimpleMatrix(inputs.length, 1, true, inputs);
-        this.target = new SimpleMatrix(numClasses, 1);
-        this.target.set(target, 1);
+    public Example(double[] input, int target, int numClasses) {
+        this.input = input;
+        this.target = new double[numClasses];
+        this.target[target] = 1;
         this.c = target;
     }
 
@@ -36,42 +28,11 @@ import org.ejml.simple.SimpleMatrix;
      * @param inputs
      * @param target
      */
-    public Example(double[] inputs, double target) {
-        this.input = inputs;
-        this.targeta = new double[1];
-        this.targeta[0] = target;
-        this.inputs = new SimpleMatrix(inputs.length, 1, true, inputs);
-        this.target = new SimpleMatrix(1, 1);
-        this.target.set(0, target);
+    public Example(double[] input, double target) {
+        this.input = input;
+        this.target = new double[1];
+        this.target[0] = target;
         this.c = target;
-    }
-
-    /**
-     * Creating a cluster from existing example
-     * @param inputs
-     */
-    public Example(SimpleMatrix inputs) {
-        this.inputs = inputs;
-    }
-
-    /**
-     * Calculates the distance from another example e
-     * @param e
-     */
-    public void distanceFrom(Example e) {
-        distance = this.inputs.minus(e.inputs).elementPower(2).elementSum();
-    }
-
-    @Override
-    public int compareTo(Example e) {
-        if (this.distance < e.distance) return -1;
-        else if (this.distance > e.distance) return 1;
-        else return 0;
-    }
-    
-    @Override
-    public String toString() {
-        return this.inputs.toString();
     }
 
  }
